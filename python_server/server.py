bc2b13b284c464f41dca6eb9c7d473468e3210af
Charles Tananbaum <charlestananbaum@gmail.com>
2018-07-27 16:10:05 -0700

    Update server.py

--- python_server/server.py
+++ python_server/server.py
@@ -6,4 +6,5 @@ def hello():
     return render_template("index.html")
 
 if __name__ == "__main__":
-	app.run(port=3500, debug=True)
+	port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
